840a802ab78c34fc422d74febcd9ba9028d75f23
shinkai09 <phyusinthaw225599@gmail.com>
2025-08-09 21:44:38 +0630

    Added .gitignore and removed unnecessary files

---
+++ CalculatorApp/CalculatorApp/Controllers/CalculatorController.cs
@@ -0,0 +1,26 @@
+﻿using CalculatorApp.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CalculatorApp.Controllers
+{
+    public class CalculatorController : Controller
+    {
+        public IActionResult Index()
+        {
+            var model = new CalculatorModel();
+            return View(model);
+        }
+
+        [HttpPost]
+        public IActionResult Calculate(CalculatorModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                model.Calculate();
+            }
+
+            return View("Index", model);
+        }
+
+    }
+}
